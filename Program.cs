5a9e5d8060f3b4c998226fb8230f0d54823554e6
Ramon Mesquita <ramonmesquitamacedo2222@gmail.com>
2023-03-31 14:31:59 -0300

    Exercício finalizado

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+string nome, sobrenome;
+Console.Write("Digite seu nome aqui:");
+nome = Console.ReadLine()!;
+Console.WriteLine();
+
+Console.Write("Digite seu sobrenome aqui:");
+sobrenome = Console.ReadLine()!;
+Console.WriteLine();
+
+Console.WriteLine($"Nome completo: {nome} {sobrenome}");
+    Console.WriteLine($"Nome de catálogo: {sobrenome.ToUpper()}, {nome}");
